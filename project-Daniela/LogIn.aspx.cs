52802fdb9cc3b506547fff7b7c628dff6dffc9c7
ddimitrova19 <danieladimitrova632@gmail.com>
2023-06-21 23:11:48 +0300

    no message

---
+++ project-Daniela/LogIn.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace project_Daniela
+{
+    SqlConnection con = new SqlConnection(connectionString: @"");
+    public partial class LogIn : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void b1_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+    }
+}
